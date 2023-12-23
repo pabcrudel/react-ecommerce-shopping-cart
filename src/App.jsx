c65afd50ab68260723d4c76220eb69b7b912150e
pabcrudel <pabcrudel@gmail.com>
2023-12-23 14:04:33 +0100

    add: Display products from mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { products } from './mocks/products.json';
+import Products from './components/Products';
+
 export default function App () {
   return (
     <>
-      <h1>React E-commerce Shopping Cart</h1>
+      <header>
+        <h1>React E-commerce Shopping Cart</h1>
+      </header>
+
+      <main>
+        <Products products={products}/>
+      </main>
     </>
   );
 }
